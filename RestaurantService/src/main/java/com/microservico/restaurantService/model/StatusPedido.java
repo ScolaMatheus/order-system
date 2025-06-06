15360ca1a52d7339cdf5357a54b1f90b8b2a4be5
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-06-06 01:34:30 -0300

    refactor: corrigindo ortografia

--- RestaurantService/src/main/java/com/microservico/restaurantService/model/StatusPedido.java
+++ RestaurantService/src/main/java/com/microservico/restaurantService/model/StatusPedido.java
@@ -1,6 +1,6 @@
     package com.microservico.restaurantService.model;
 
     public enum StatusPedido {
-        CRIADO, PREPARANDO, PRONTO, EM_ROTA, ENTREGE, CANCELADO;
+        CRIADO, PREPARANDO, PRONTO, EM_ROTA, ENTREGUE, CANCELADO;
     }
 
