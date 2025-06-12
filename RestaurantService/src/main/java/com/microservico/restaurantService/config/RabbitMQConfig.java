ddf7827b164fec8fb0d1f6a50dbf39419ec24dcb
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-06-12 02:55:44 -0300

    chore(restaurant-service): Adicionar configuração e constantes do RabbitMQ

--- RestaurantService/src/main/java/com/microservico/restaurantService/config/RabbitMQConfig.java
+++ RestaurantService/src/main/java/com/microservico/restaurantService/config/RabbitMQConfig.java
@@ -7,6 +7,8 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static com.microservico.restaurantService.util.RabbitConstants.*;
+
 @Configuration
 public class RabbitMQConfig {
 
@@ -29,8 +31,59 @@ public class RabbitMQConfig {
             message.getMessageProperties().setDeliveryMode(MessageDeliveryMode.PERSISTENT);
             return message;
         });
-
         return template;
-
     }
+
+    // Exchange do tipo topic
+    @Bean
+    public TopicExchange pedidoExchange() {
+        return new TopicExchange(PEDIDO_EXCHANGE);
+    }
+
+    // Fila que vai receber os eventos do pedido criado
+    @Bean
+    public Queue pedidoQueue() {
+        return definirFila(PEDIDO_QUEUE);
+    }
+
+    // Fila que vai receber os eventos dos pedidos cancelados
+    @Bean
+    public Queue pedidoCanceladoQueue() {
+        return definirFila(PEDIDO_CANCELADO_QUEUE);
+    }
+
+    // Fila que vai receber os eventos dos pedidos entregues
+    @Bean
+    public Queue pedidoEntregueQueue() {
+        return definirFila(PEDIDO_ENTREGUE_QUEUE);
+    }
+
+    // Ligação entre a exchange e a fila com uma routing key (pedidos criados)
+    @Bean
+    public Binding pedidoBinding(Queue pedidoQueue, TopicExchange pedidoExchange) {
+        return definirBindings(pedidoQueue, pedidoExchange, PEDIDO_ROUTING_KEY);
+    }
+
+    // Ligação entre a exchange e a fila com uma routing key (pedidos cancelados)
+    @Bean
+    public Binding pedidoCanceladoBinding(Queue pedidoCanceladoQueue, TopicExchange pedidoExchange) {
+        return definirBindings(pedidoCanceladoQueue, pedidoExchange, PEDIDO_CANCELADO_ROUTING_KEY);
+    }
+
+    // Ligação entre a exchange e a fila com uma routing key (pedidos entregues)
+    @Bean
+    public Binding pedidoEntregueBinding(Queue pedidoEntregueQueue, TopicExchange pedidoExchange) {
+        return definirBindings(pedidoEntregueQueue, pedidoExchange, PEDIDO_ENTREGUE_ROUTING_KEY);
+    }
+
+    private Queue definirFila(String nameQueue) {
+        return QueueBuilder.durable(nameQueue).build();
+    }
+
+    private Binding definirBindings(Queue queue, TopicExchange topicExchange, String routingKey) {
+        return BindingBuilder.bind(queue)
+                .to(topicExchange)
+                .with(routingKey);
+    }
+
 }
--- RestaurantService/src/main/java/com/microservico/restaurantService/util/RabbitConstants.java
+++ RestaurantService/src/main/java/com/microservico/restaurantService/util/RabbitConstants.java
@@ -2,14 +2,16 @@ package com.microservico.restaurantService.util;
 
 public class RabbitConstants {
 
-    public static final String PEDIDO_EXCHANGE = "pedido.exchange";
-    public static final String PEDIDO_QUEUE = "pedido.criado.queue";
-    public static final String PEDIDO_PREPARANDO_QUEUE = "pedido.preparando.queue";
-    public static final String PEDIDO_EM_ROTA_QUEUE = "pedido.em-rota.queue";
-    public static final String PEDIDO_ENTREGE_QUEUE = "pedido.entrege.queue";
-    public static final String PEDIDO_CANCELADO_ROUTING_KEY = "pedido.cancelado";
-    public static final String PEDIDO_PREPARANDO_ROUTING_KEY = "pedido.preparando";
-    public static final String PEDIDO_EM_ROTA_ROUTING_KEY = "pedido.em-rota";
-    public static final String PEDIDO_ENTREGE_ROUTING_KEY = "pedido.entrege";
+    public static final String PEDIDO_EXCHANGE = "pedido.status.exchange";
+
+    public static final String PEDIDO_QUEUE = "pedido.status.criado.restaurant.queue";
+    public static final String PEDIDO_CANCELADO_QUEUE = "pedido.status.cancelado.restaurant.queue";
+    public static final String PEDIDO_ENTREGUE_QUEUE = "pedido.status.entregue.restaurant.queue";
+
+    public static final String PEDIDO_ROUTING_KEY = "pedido.status.criado";
+    public static final String PEDIDO_CANCELADO_ROUTING_KEY = "pedido.status.cancelado";
+    public static final String PEDIDO_PREPARANDO_ROUTING_KEY = "pedido.status.preparando";
+    public static final String PEDIDO_EM_ROTA_ROUTING_KEY = "pedido.status.em-rota";
+    public static final String PEDIDO_ENTREGUE_ROUTING_KEY = "pedido.status.entregue";
 
 }
