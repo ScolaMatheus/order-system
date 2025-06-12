54ebb57cb599dc43f3488fada4f02a2fd43a48d4
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-06-12 03:00:32 -0300

    refactor(customer-service): Ajustar modelagem da entidade Pedido

---
+++ customerservice/src/main/java/com/microservico/customerservice/model/Pedido.java
@@ -0,0 +1,56 @@
+package com.microservico.customerservice.model;
+
+import com.microservico.customerservice.util.StatusPedido;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private Long clienteId;
+
+    @Column(nullable = false)
+    private Long restauranteId;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ItemPedido> itens;
+
+    @CreationTimestamp
+    @Column(name = "criado_em")
+    private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    @Column(name = "atualizado_em")
+    private LocalDateTime dataAtualizacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "status")
+    private StatusPedido statusPedido;
+
+    @Column(nullable = false)
+    private Double valorTotal;
+
+    public Double getValorTotal() {
+        if (itens == null) return 0.0;
+        return itens.stream()
+                .mapToDouble(ItemPedido::getValorTotal)
+                .sum();
+    }
+}
