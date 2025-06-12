82815c5edc51f7ebe245207063dcc1184e7b3590
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-06-12 02:48:37 -0300

    feat(customer-service): Adicionar publisher e evento para cancelamento de pedidos

---
+++ customerservice/src/main/java/com/microservico/customerservice/event/PedidoCanceladoEvent.java
@@ -0,0 +1,33 @@
+package com.microservico.customerservice.event;
+
+import com.microservico.customerservice.util.StatusPedido;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class PedidoCanceladoEvent extends PedidoEvent{
+    private String motivoCancelamento;
+    private String origemCancelamento;
+
+    public PedidoCanceladoEvent(
+            Long pedidoId,
+            Long restauranteId,
+            Long clienteId,
+            StatusPedido statusPedido,
+            LocalDateTime dataHoraAtualizacao,
+            String motivoCancelamento,
+            String origemCancelamento
+    ) {
+        super(pedidoId, restauranteId, clienteId, statusPedido, dataHoraAtualizacao);
+        this.motivoCancelamento = motivoCancelamento;
+        this.origemCancelamento = origemCancelamento;
+    }
+
+}
---
+++ customerservice/src/main/java/com/microservico/customerservice/event/PedidoEvent.java
@@ -0,0 +1,20 @@
+package com.microservico.customerservice.event;
+
+
+import com.microservico.customerservice.util.StatusPedido;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PedidoEvent {
+    private Long pedidoId;
+    private Long restauranteId;
+    private Long clienteId;
+    private StatusPedido statusPedido;
+    private LocalDateTime dataHoraAtualizacao;
+}
