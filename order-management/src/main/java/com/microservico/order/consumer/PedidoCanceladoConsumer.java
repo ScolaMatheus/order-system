18451a752528928f619398dacaab754f68a20215
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-07-01 20:34:47 -0300

    feat(order-management): tratar retry manual em PedidoCanceladoConsumer

--- order-management/src/main/java/com/microservico/order/consumer/PedidoCanceladoConsumer.java
+++ order-management/src/main/java/com/microservico/order/consumer/PedidoCanceladoConsumer.java
@@ -3,8 +3,10 @@ package com.microservico.order.consumer;
 import com.microservico.order.event.PedidoCanceladoEvent;
 import com.microservico.order.model.StatusPedido;
 import com.microservico.order.service.PedidoService;
+import com.microservico.order.util.RabbitUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +15,21 @@ import static com.microservico.order.util.RabbitConstants.PEDIDO_CANCELADO_QUEUE
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class PedidoCanceladoConsumer {
+public class PedidoCanceladoConsumer extends RabbitUtil {
 
     private final PedidoService pedidoService;
 
     @RabbitListener(queues = PEDIDO_CANCELADO_QUEUE)
-    public void consumirPedidoCancelado(PedidoCanceladoEvent event) {
+    public void consumirPedidoCancelado(PedidoCanceladoEvent event, Message message) {
+        int tentativas = getTentativas(message);
         log.info("Pedido cancelado recebido: {}", event);
+
+        // Em caso de falha no consumo da mensagem ela será reprocessada 3 vezes, após isso ela será descartada
+        if (tentativas >= 3) {
+            log.warn("Mensagem ignorada após {} tentativas: {}", tentativas, event);
+            return;
+        }
+
         pedidoService.atualizarPedido(event.getPedidoId(), StatusPedido.CANCELADO,event.getDataHoraAtualizacao());
 
     }
