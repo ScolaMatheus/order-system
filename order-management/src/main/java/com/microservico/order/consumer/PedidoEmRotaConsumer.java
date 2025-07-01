2d849dd3aeb5b6d172b6b471f4ce0705a3292809
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-07-01 20:35:22 -0300

    feat(order-management): tratar retry manual em PedidoEmRotaConsumer

--- order-management/src/main/java/com/microservico/order/consumer/PedidoEmRotaConsumer.java
+++ order-management/src/main/java/com/microservico/order/consumer/PedidoEmRotaConsumer.java
@@ -3,8 +3,10 @@ package com.microservico.order.consumer;
 import com.microservico.order.event.PedidoStatusEvent;
 import com.microservico.order.model.StatusPedido;
 import com.microservico.order.service.PedidoService;
+import com.microservico.order.util.RabbitUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +15,21 @@ import static com.microservico.order.util.RabbitConstants.PEDIDO_EM_ROTA_QUEUE;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class PedidoEmRotaConsumer {
+public class PedidoEmRotaConsumer extends RabbitUtil {
 
     private final PedidoService pedidoService;
 
     @RabbitListener(queues = PEDIDO_EM_ROTA_QUEUE)
-    public void consumirPedidoEmRota(PedidoStatusEvent event) {
+    public void consumirPedidoEmRota(PedidoStatusEvent event, Message message) {
+        int tentativas = getTentativas(message);
         log.info("Pedido em rota de entrega recebido: {}", event);
+
+        // Em caso de falha no consumo da mensagem ela será reprocessada 3 vezes, após isso ela será descartada
+        if (tentativas >= 3) {
+            log.warn("Mensagem ignorada após {} tentativas: {}", tentativas, event);
+            return;
+        }
+
         pedidoService.atualizarPedido(event.getPedidoId(), StatusPedido.EM_ROTA, event.getDataHoraAtualizacao());
     }
 
