25d5fad629f4a3fc9e5d1e1638dcd40c3d18924e
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-07-02 17:50:06 -0300

    feat(customer-service): tratar retry manual em PedidoEmRotaConsumer

--- customer-service/src/main/java/com/microservico/customer/consumer/PedidoEmRotaConsumer.java
+++ customer-service/src/main/java/com/microservico/customer/consumer/PedidoEmRotaConsumer.java
@@ -2,9 +2,11 @@ package com.microservico.customer.consumer;
 
 import com.microservico.customer.event.PedidoStatusEvent;
 import com.microservico.customer.service.PedidoService;
+import com.microservico.customer.util.RabbitUtil;
 import com.microservico.customer.util.StatusPedido;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -14,18 +16,21 @@ import static com.microservico.customer.util.RabbitConstants.PEDIDO_EM_ROTA_QUEU
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class PedidoEmRotaConsumer {
+public class PedidoEmRotaConsumer extends RabbitUtil {
 
     private final PedidoService pedidoService;
 
     @RabbitListener(queues = PEDIDO_EM_ROTA_QUEUE)
-    public void consumirPedidoEmRota(PedidoStatusEvent event) {
+    public void consumirPedidoEmRota(PedidoStatusEvent event, Message message) {
+        int tentativas = getTentativas(message);
         log.info("Pedido em rota de entrega recebido: {}", event);
-        try {
-            pedidoService.atualizarPedido(event.getPedidoId(), StatusPedido.EM_ROTA, event.getDataHoraAtualizacao());
-        } catch (Exception e) {
-            log.error("Erro ao processar evento de pedido [{}]: {}", event.getPedidoId(), e.getMessage(), e);
+
+        if (tentativas > 3) {
+            log.warn("Mensagem ignorada após {} tentativas: {}", tentativas, event);
+            return;
         }
+
+        pedidoService.atualizarPedido(event.getPedidoId(), StatusPedido.EM_ROTA, event.getDataHoraAtualizacao());
 
     }
 
