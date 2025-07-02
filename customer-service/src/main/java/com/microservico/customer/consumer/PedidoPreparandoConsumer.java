73c8d221a05123b1a2369d10d72df993a22118bc
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-07-02 17:50:27 -0300

    feat(customer-service): tratar retry manual em PedidoPreparandoConsumer

--- customer-service/src/main/java/com/microservico/customer/consumer/PedidoPreparandoConsumer.java
+++ customer-service/src/main/java/com/microservico/customer/consumer/PedidoPreparandoConsumer.java
@@ -2,9 +2,11 @@ package com.microservico.customer.consumer;
 
 import com.microservico.customer.event.PedidoStatusEvent;
 import com.microservico.customer.service.PedidoService;
+import com.microservico.customer.util.RabbitUtil;
 import com.microservico.customer.util.StatusPedido;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -14,17 +16,20 @@ import static com.microservico.customer.util.RabbitConstants.PEDIDO_PREPARANDO_Q
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class PedidoPreparandoConsumer {
+public class PedidoPreparandoConsumer extends RabbitUtil {
 
     private final PedidoService pedidoService;
 
     @RabbitListener(queues = PEDIDO_PREPARANDO_QUEUE)
-    public void consumirPedidoEmPreparo(PedidoStatusEvent event) {
+    public void consumirPedidoEmPreparo(PedidoStatusEvent event, Message message) {
+        int tentativas = getTentativas(message);
         log.info("Pedido em preparo recebido: {}", event);
-        try {
-            pedidoService.atualizarPedido(event.getPedidoId(), StatusPedido.PREPARANDO, event.getDataHoraAtualizacao());
-        } catch (Exception e) {
-            log.error("Erro ao processar evento de pedido [{}]: {}", event.getPedidoId(), e.getMessage(), e);
+
+        if (tentativas > 3) {
+            log.warn("Mensagem ignorada após {} tentativas: {}", tentativas, event);
+            return;
         }
+
+        pedidoService.atualizarPedido(event.getPedidoId(), StatusPedido.PREPARANDO, event.getDataHoraAtualizacao());
     }
 }
