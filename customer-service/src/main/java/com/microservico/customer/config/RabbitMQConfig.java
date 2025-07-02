e07e8075185374444593ce8d0e787f928457d48c
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-07-02 17:52:24 -0300

    feat(customer-service): configurar filas com DLX e retry via TTL

--- customer-service/src/main/java/com/microservico/customer/config/RabbitMQConfig.java
+++ customer-service/src/main/java/com/microservico/customer/config/RabbitMQConfig.java
@@ -7,6 +7,9 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.microservico.customer.util.RabbitConstants.*;
 
 @Configuration
@@ -40,39 +43,98 @@ public class RabbitMQConfig {
         return new TopicExchange(PEDIDO_EXCHANGE);
     }
 
+    // Exchange do tipo topic reprocessar as mensagens que falharam
+    @Bean
+    public TopicExchange pedidoExchangeDlx() {
+        return new TopicExchange(PEDIDO_EXCHANGE_DLX);
+    }
+
     // Fila que vai receber os eventos dos pedidos cancelados
     @Bean
     public Queue pedidoCanceladoQueue() {
-        return definirFila(PEDIDO_CANCELADO_QUEUE);
+        return definirFilaComDlx(PEDIDO_CANCELADO_QUEUE, PEDIDO_CANCELADO_RETRY_ROUTING_KEY);
+    }
+
+    // Fila que vai receber os eventos dos pedidos cancelados que falharam no consumo
+    @Bean
+    public Queue pedidoCanceladoQueueDlq() {
+        return definirFilaRetry(PEDIDO_CANCELADO_QUEUE_DLQ, PEDIDO_CANCELADO_ROUTING_KEY_CUSTOMER);
     }
 
     // Fila que vai receber os eventos dos pedidos em preparação
     @Bean
     public Queue pedidoPreparandoQueue() {
-        return definirFila(PEDIDO_PREPARANDO_QUEUE);
+        return definirFilaComDlx(PEDIDO_PREPARANDO_QUEUE, PEDIDO_PREPARANDO_RETRY_ROUTING_KEY);
+    }
+
+    // Fila que vai receber os eventos dos pedidos em preparação que falharam no consumo
+    @Bean
+    public Queue pedidoPreparandoQueueDlq() {
+        return definirFilaRetry(PEDIDO_PREPARANDO_QUEUE_DLQ, PEDIDO_PREPARANDO_ROUTING_KEY);
     }
 
     // Fila que vai receber os eventos dos pedidos em rota de entrega
     @Bean
     public Queue pedidoEmRotaQueue() {
-        return definirFila(PEDIDO_EM_ROTA_QUEUE);
+        return definirFilaComDlx(PEDIDO_EM_ROTA_QUEUE, PEDIDO_EM_ROTA_RETRY_ROUTING_KEY);
     }
 
+    // Fila que vai receber os eventos dos pedidos em rota que falharam no consumo
+    @Bean
+    public Queue pedidoEmRotaQueueDlq() {
+        return definirFilaRetry(PEDIDO_EM_ROTA_QUEUE_DLQ, PEDIDO_EM_ROTA_ROUTING_KEY);
+    }
+
+    // Ligação entre a exchange e a fila com uma routing key
     @Bean
     public Binding pedidoCanceladoBinding(Queue pedidoCanceladoQueue, TopicExchange pedidoExchange) {
-        return definirBindings(pedidoCanceladoQueue, pedidoExchange, PEDIDO_CANCELADO_ROUTING_KEY);
+        return definirBindings(pedidoCanceladoQueue, pedidoExchange, PEDIDO_CANCELADO_ROUTING_KEY_ALL);
     }
+
+    // Ligação entre a exchange e a fila com uma routing key para caso de falhas no consumo
+    @Bean
+    public Binding pedidoCanceladoBindingDlq(Queue pedidoCanceladoQueueDlq, TopicExchange pedidoExchangeDlx) {
+        return definirBindings(pedidoCanceladoQueueDlq, pedidoExchangeDlx, PEDIDO_CANCELADO_RETRY_ROUTING_KEY);
+    }
+
+    // Ligação entre a exchange e a fila com uma routing key
     @Bean
     public Binding pedidoPreparandoBinding(Queue pedidoPreparandoQueue, TopicExchange pedidoExchange) {
         return definirBindings(pedidoPreparandoQueue, pedidoExchange, PEDIDO_PREPARANDO_ROUTING_KEY);
     }
+
+    // Ligação entre a exchange e a fila com uma routing key para caso de falhas no consumo
+    @Bean
+    public Binding pedidoPreparandoBindingDlq(Queue pedidoPreparandoQueueDlq, TopicExchange pedidoExchangeDlx) {
+        return definirBindings(pedidoPreparandoQueueDlq, pedidoExchangeDlx, PEDIDO_PREPARANDO_RETRY_ROUTING_KEY);
+    }
+
+    // Ligação entre a exchange e a fila com uma routing key
     @Bean
     public Binding pedidoEmRotaBinding(Queue pedidoEmRotaQueue, TopicExchange pedidoExchange) {
         return definirBindings(pedidoEmRotaQueue, pedidoExchange, PEDIDO_EM_ROTA_ROUTING_KEY);
     }
 
-    private Queue definirFila(String nameQueue) {
-        return QueueBuilder.durable(nameQueue).build();
+    // Ligação entre a exchange e a fila com uma routing key para caso de falhas no consumo
+    @Bean
+    public Binding pedidoEmRotaBindingDlq(Queue pedidoEmRotaQueueDlq, TopicExchange pedidoExchangeDlx) {
+        return definirBindings(pedidoEmRotaQueueDlq, pedidoExchangeDlx, PEDIDO_EM_ROTA_RETRY_ROUTING_KEY);
+    }
+
+    private Queue definirFilaComDlx(String nameQueue, String retryRoutingKey) {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", PEDIDO_EXCHANGE_DLX);
+        args.put("x-dead-letter-routing-key", retryRoutingKey);
+
+        return QueueBuilder.durable(nameQueue).withArguments(args).build();
+    }
+
+    private Queue definirFilaRetry(String retryQueueName, String nameRoutingKey) {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-message-ttl", 8000);
+        args.put("x-dead-letter-exchange", PEDIDO_EXCHANGE);
+        args.put("x-dead-letter-routing-key", nameRoutingKey);
+        return QueueBuilder.durable(retryQueueName).withArguments(args).build();
     }
 
     private Binding definirBindings(Queue queue, TopicExchange topicExchange, String routingKey) {
--- customer-service/src/main/java/com/microservico/customer/util/RabbitConstants.java
+++ customer-service/src/main/java/com/microservico/customer/util/RabbitConstants.java
@@ -2,16 +2,33 @@ package com.microservico.customer.util;
 
 public class RabbitConstants {
 
+    // Nome das Exchanges
     public static final String PEDIDO_EXCHANGE = "pedido.status.exchange";
+    public static final String PEDIDO_EXCHANGE_DLX = "pedido.status.exchange.dlx";
 
-    public static final String PEDIDO_QUEUE = "pedido.status.criado.customer.queue";
+    // Nome das filas
+    public static final String PEDIDO_CRIADO_QUEUE = "pedido.status.criado.customer.queue";
     public static final String PEDIDO_CANCELADO_QUEUE = "pedido.status.cancelado.customer.queue";
     public static final String PEDIDO_PREPARANDO_QUEUE = "pedido.status.preparando.customer.queue";
     public static final String PEDIDO_EM_ROTA_QUEUE = "pedido.status.em-rota.customer.queue";
 
-    public static final String PEDIDO_ROUTING_KEY = "pedido.status.criado";
+    // Nome das filas para processar as falhas do consumo das filas originais
+    public static final String PEDIDO_CANCELADO_QUEUE_DLQ = "pedido.status.cancelado.customer.queue.dlq";
+    public static final String PEDIDO_PREPARANDO_QUEUE_DLQ = "pedido.status.preparando.customer.queue.dlq";
+    public static final String PEDIDO_EM_ROTA_QUEUE_DLQ = "pedido.status.em-rota.customer.queue.dlq";
+
+    // Palavras-chave para as filas
+    public static final String PEDIDO_CRIADO_ROUTING_KEY = "pedido.status.criado";
+    public static final String PEDIDO_CANCELADO_ROUTING_KEY_CUSTOMER = "pedido.status.cancelado.customer";
+    public static final String PEDIDO_CANCELADO_ROUTING_KEY_ALL = "pedido.status.cancelado.*";
     public static final String PEDIDO_CANCELADO_ROUTING_KEY = "pedido.status.cancelado";
     public static final String PEDIDO_PREPARANDO_ROUTING_KEY = "pedido.status.preparando";
     public static final String PEDIDO_EM_ROTA_ROUTING_KEY = "pedido.status.em-rota";
     public static final String PEDIDO_ENTREGUE_ROUTING_KEY = "pedido.status.entregue";
+
+    // Palavras-chave para as filas de retry
+    public static final String PEDIDO_CANCELADO_RETRY_ROUTING_KEY = "pedido.status.cancelado.retry";
+    public static final String PEDIDO_PREPARANDO_RETRY_ROUTING_KEY = "pedido.status.preparando.retry";
+    public static final String PEDIDO_EM_ROTA_RETRY_ROUTING_KEY = "pedido.status.em-rota.retry";
+    public static final String PEDIDO_ENTREGUE_RETRY_ROUTING_KEY = "pedido.status.entregue.retry";
 }
