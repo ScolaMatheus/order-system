c6e6f4865fd3fbfcec883b3a4219b50288bf10d3
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-07-03 11:22:59 -0300

    feat(restaurant-service): tratar retry manual em PedidoCriadoConsumer

--- restaurant-service/src/main/java/com/microservico/restaurant/consumer/PedidoCriadoConsumer.java
+++ restaurant-service/src/main/java/com/microservico/restaurant/consumer/PedidoCriadoConsumer.java
@@ -2,24 +2,34 @@ package com.microservico.restaurant.consumer;
 
 import com.microservico.restaurant.event.PedidoStatusEvent;
 import com.microservico.restaurant.service.PedidoService;
+import com.microservico.restaurant.util.RabbitUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import static com.microservico.restaurant.util.RabbitConstants.PEDIDO_QUEUE;
+import static com.microservico.restaurant.util.RabbitConstants.PEDIDO_CRIADO_QUEUE;
 
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class PedidoCriadoConsumer {
+public class PedidoCriadoConsumer extends RabbitUtil {
 
     private final PedidoService pedidoService;
 
-    @RabbitListener(queues = PEDIDO_QUEUE)
-    public void consumirPedido(PedidoStatusEvent event) {
-        log.info("Pedido recebido: {}",  event);
+    @RabbitListener(queues = PEDIDO_CRIADO_QUEUE)
+    public void consumirPedido(PedidoStatusEvent event, Message message) {
+        int tentativas = getTentativas(message);
+
+        if (tentativas >= 3) {
+            log.warn("Mensagem ignorada após {} tentativas: {}", tentativas, event);
+            return;
+        }
+
+        log.info("Pedido criado recebido: {}",  event);
+
         pedidoService.consumirPedido(event);
     }
 }
