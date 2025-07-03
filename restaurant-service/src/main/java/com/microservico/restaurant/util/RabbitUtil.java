4ef618b7f8b46c4f39b3474cf969564923eb0947
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-07-03 11:20:43 -0300

    feat(restaurant-service): adicionar utilitário para leitura de tentativas via x-death

---
+++ restaurant-service/src/main/java/com/microservico/restaurant/util/RabbitUtil.java
@@ -0,0 +1,31 @@
+package com.microservico.restaurant.util;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class RabbitUtil {
+
+    public int getTentativas(Message message) {
+        Map<String, Object> headers = message.getMessageProperties().getHeaders();
+
+        if (headers.containsKey("x-death")) {
+            Object xDeath = headers.get("x-death");
+
+            if (xDeath instanceof List<?> list) {
+                Object first = list.get(0);
+                if (first instanceof Map<?, ?> map) {
+                    Object count = map.get("count");
+                    if (count instanceof Long) {
+                        return ((Long) count).intValue();
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+}
