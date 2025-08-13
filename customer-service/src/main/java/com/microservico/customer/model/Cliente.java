7cbda167a23b40cd136fdadbb6b41e5df80f3192
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-08-12 22:52:51 -0300

    refactor(customer-service): adaptar Cliente | ItemPedido | Pedido para arquitetura hexagonal

--- customer-service/src/main/java/com/microservico/customer/model/Cliente.java
+++ customer-service/src/main/java/com/microservico/customer/model/Cliente.java
@@ -1,34 +1,64 @@
 package com.microservico.customer.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
-import lombok.*;
-
-@Entity
-@Table(name = "tb_cliente")
-@Getter
-@Setter
-@AllArgsConstructor
-@EqualsAndHashCode
-@NoArgsConstructor
-@ToString
 public class Cliente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
-    @Column(nullable = false)
     private String nome;
 
-    @Pattern(regexp = "\\d{11}", message = "CPF deve conter 11 dígitos numéricos")
-    @Column(length = 11, unique = true, nullable = false)
     private String cpf;
 
-    @Column(length = 15, nullable = false)
     private String telefone;
 
-    @Column(unique = true, nullable = false)
-    @Email
     private String email;
+
+    public Cliente() {}
+
+    public Cliente(Long id, String email, String nome, String cpf, String telefone) {
+        this.id = id;
+        this.email = email;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
