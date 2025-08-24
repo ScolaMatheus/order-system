48cdfa2f7ca8c619b9bb2a249349e923df231f29
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-08-24 01:16:21 -0300

    refactor(order): PedidoRepository

---
+++ order-management/src/main/java/com/microservico/order/application/repository/PedidoRepository.java
@@ -0,0 +1,14 @@
+package com.microservico.order.application.repository;
+
+import com.microservico.order.model.Pedido;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PedidoRepository {
+
+    void save(Pedido pedido);
+    List<Pedido> findAll();
+    Optional<Pedido> findById(Long idPedido);
+    void delete(Long id);
+}
