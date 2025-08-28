5e6cff94a33b3136d17db1eea76eaea6243f6f22
Matheus Fernandes <matheus.fernan01@gmail.com>
2025-08-28 02:11:21 -0300

    feat(customer): remover lógica do RabbitMQ e incluir lógica do Kafka para publicação dos eventos

--- customer-service/src/main/java/com/microservico/customer/adapter/outbound/publisher/PedidoEventPublisher.java
+++ customer-service/src/main/java/com/microservico/customer/adapter/outbound/publisher/PedidoEventPublisher.java
@@ -4,49 +4,35 @@ import com.microservico.customer.application.publisher.IPedidoEventPublisher;
 import com.microservico.customer.event.PedidoCanceladoEvent;
 
 import com.microservico.customer.event.PedidoStatusEvent;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-
-import static com.microservico.customer.util.RabbitConstants.*;
-
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class PedidoEventPublisher implements IPedidoEventPublisher {
 
-    private final RabbitTemplate rabbitTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public PedidoEventPublisher(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     @Override
     public void publicarPedidoCancelado(PedidoCanceladoEvent event) {
-        rabbitTemplate.convertAndSend(
-                PEDIDO_EXCHANGE,
-                PEDIDO_CANCELADO_ROUTING_KEY_CUSTOMER,
-                event
-        );
+        kafkaTemplate.send("pedido-cancelado-group", event);
         log.info("PedidoCanceladoEvent publicado : {}", event);
     }
 
     @Override
     public void publicarPedidoEntregue(PedidoStatusEvent pedidoEvent) {
-        rabbitTemplate.convertAndSend(
-                PEDIDO_EXCHANGE,
-                PEDIDO_ENTREGUE_ROUTING_KEY,
-                pedidoEvent
-        );
+        kafkaTemplate.send("pedido-entregue-group", pedidoEvent);
         log.info("PedidoStatusEvent {} foi entregue ao cliente {}", pedidoEvent, pedidoEvent.getClienteId());
     }
 
     @Override
     public void publicarPedidoCriado(PedidoStatusEvent pedidoEvent) {
-        rabbitTemplate.convertAndSend(
-                PEDIDO_EXCHANGE,
-                PEDIDO_CRIADO_ROUTING_KEY,
-                pedidoEvent
-        );
+        kafkaTemplate.send("pedido-criado-group", pedidoEvent);
         log.info("PedidoStatusEvent {} foi criado no customer-service", pedidoEvent);
     }
 }
